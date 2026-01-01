c94ff1fa9bdc750c6e3d917065bb4839e2c722e2
Duong Nguyen <chieuduong2311@hotmail.com>
2026-01-01 23:28:34 +0700

    issue 704 week 1

--- src/src/algorithm/week1/BinarySearch.java
+++ src/src/algorithm/week1/BinarySearch.java
@@ -1,8 +1,23 @@
 package algorithm.week1;
 
 public class BinarySearch {
-//    issue 704
+    //    issue 704
     public int search(int[] nums, int target) {
-        return 0;
+        int start = 0, end = nums.length - 1;
+        int result = (end - start + 1) / 2;
+        while (start < end) {
+            if (nums[result] == target) {
+                return result;
+            } else if (nums[result] > target) {
+                end = result - 1;
+                result = (end - start + 1) / 2;
+            } else {
+                start = result + 1;
+                result = result + (end - start + 1) / 2;
+            }
+        }
+        if (nums[end] == target) {
+            return end;
+        } else return -1;
     }
 }
