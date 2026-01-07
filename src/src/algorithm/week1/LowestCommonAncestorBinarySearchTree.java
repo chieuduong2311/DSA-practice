007f9f22939c7f8e3b1c12aa918202831000369f
Duong Nguyen <chieuduong2311@hotmail.com>
2026-01-07 20:28:13 +0700

    issue 235 week 1

--- src/src/algorithm/week1/LowestCommonAncestorBinarySearchTree.java
+++ src/src/algorithm/week1/LowestCommonAncestorBinarySearchTree.java
@@ -10,6 +10,40 @@ public class LowestCommonAncestorBinarySearchTree {
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (root.val > p.val && root.val > q.val) {
+                root = root.left;
+                continue;
+            }
+            if (root.val < p.val && root.val < q.val){
+                root = root.right;
+                continue;
+            }
+            return root;
+        }
         return null;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(8);
+        TreeNode n3 = new TreeNode(0);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(3);
+        TreeNode n6 = new TreeNode(5);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n8 = new TreeNode(9);
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n1.right = n4;
+        n4.left = n5;
+        n4.right = n6;
+        n2.left = n7;
+        n2.right = n8;
+
+        LowestCommonAncestorBinarySearchTree l = new LowestCommonAncestorBinarySearchTree();
+        System.out.println(l.lowestCommonAncestor(root, new TreeNode(3), new TreeNode(4)).val);
     }
 }
