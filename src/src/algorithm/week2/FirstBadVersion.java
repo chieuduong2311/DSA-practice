727eb59ae102e86102c834676015510492cab157
Duong Nguyen <chieuduong2311@hotmail.com>
2026-01-13 00:39:08 +0700

    issue 278 week2

--- src/src/algorithm/week2/FirstBadVersion.java
+++ src/src/algorithm/week2/FirstBadVersion.java
@@ -3,6 +3,29 @@ package algorithm.week2;
 public class FirstBadVersion {
 //    issue 278
     public int firstBadVersion(int n) {
-        return 0;
+        if (isBadVersion(1)) {
+            return 1;
+        }
+        int start = 1;
+        int end = n;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return end;
     }
+    
+    boolean isBadVersion(int version) {
+        return version >= 9;
+    }
+
+    public static void main(String[] args) {
+        FirstBadVersion firstBadVersion = new FirstBadVersion();
+        System.out.println(firstBadVersion.firstBadVersion(10));
+    }
+
 }
