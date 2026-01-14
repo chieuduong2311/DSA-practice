b0397c440887a63e2aa12d0884ff64bdd37673a0
Duong Nguyen <chieuduong2311@hotmail.com>
2026-01-14 23:34:31 +0700

    issue 242 week1

--- src/src/algorithm/week1/ValidAnagram.java
+++ src/src/algorithm/week1/ValidAnagram.java
@@ -9,19 +9,45 @@ public class ValidAnagram {
         if (s.length() != t.length()) {
             return false;
         }
+//        Map<Character, Integer> countMap = new HashMap<>();
+//        for (int i = 0; i < s.length(); i++) {
+//            countMap.merge(s.charAt(i), 1, Integer::sum);
+//        }
+//
+//        for (int i = 0; i < t.length(); i++) {
+//            Integer value = countMap.get(t.charAt(i));
+//            if (value == null || value == 0) {
+//                return false;
+//            }
+//            countMap.put(t.charAt(i), value - 1);
+//        }
+//
+//        return true;
+
         Map<Character, Integer> countMap = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
-            countMap.merge(s.charAt(i), 1, Integer::sum);
+
+            countMap.putIfAbsent(s.charAt(i), 0);
+            countMap.compute(s.charAt(i), (k,v) -> ++v);
+
+            countMap.putIfAbsent(t.charAt(i), 0);
+            countMap.compute(t.charAt(i), (k,v) -> --v);
+
         }
 
-        for (int i = 0; i < t.length(); i++) {
-            Integer value = countMap.get(t.charAt(i));
-            if (value == null || value == 0) {
+        for (Integer count : countMap.values()) {
+            if (count != 0) {
                 return false;
             }
-            countMap.put(t.charAt(i), value - 1);
         }
-
         return true;
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram validAnagram = new ValidAnagram();
+        var s = "anagram";
+        var t = "nagaram";
+
+        System.out.println(validAnagram.isAnagram(s, t));
     }
 }
