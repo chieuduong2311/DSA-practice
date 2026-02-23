1f35ea7340d2f8d5eb032731d4544e8f3fa2e7ca
Duong Nguyen <chieuduong2311@hotmail.com>
2026-02-23 21:31:28 +0700

    issue 70 & 409 week2

--- src/src/algorithm/week2/ClimbingStairs.java
+++ src/src/algorithm/week2/ClimbingStairs.java
@@ -3,6 +3,15 @@ package algorithm.week2;
 public class ClimbingStairs {
 //    issue 70
     public int climbStairs(int n) {
-        return 0;
+        if (n <= 3) {
+            return n;
+        }
+        int n1 = 2, n2 = 3, n3 = 0;
+        for (int i = 4; i <= n ; i++) {
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
     }
 }
--- src/src/algorithm/week2/LongestPalindrome.java
+++ src/src/algorithm/week2/LongestPalindrome.java
@@ -3,6 +3,26 @@ package algorithm.week2;
 public class LongestPalindrome {
 //    issue 409
     public int longestPalindrome(String s) {
-        return 0;
+        if (s.length() <= 1) {
+            return s.length();
+        }
+        int[] count = new int[52];
+        for (int i = 0; i < s.length() ; i++) {
+            if (s.charAt(i) >= 97) {
+                count[s.charAt(i) - 'a' + 26]++;
+            } else count[s.charAt(i) - 'A']++;
+        }
+
+        boolean hasOdd = false;
+        int total = 0;
+        for (int c : count) {
+            if (c % 2 == 0) {
+                total += c;
+                continue;
+            }
+            hasOdd = true;
+            total += c - 1;
+        }
+        return hasOdd ? total + 1 : total;
     }
 }
