881a88b131913bbae8b90be5014686b0b379c032
Duong Nguyen <chieuduong2311@hotmail.com>
2026-06-21 19:15:01 +0700

    complete 1375 - not optimized 208 trie - still failed last case

---
+++ src/src/algorithm/extra/BinaryStringPrefixAligned.java
@@ -0,0 +1,30 @@
+package algorithm.extra;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BinaryStringPrefixAligned {
+//    issue 1375
+    public int numTimesAllBlue(int[] flips) {
+        boolean[] visited = new boolean[flips.length];
+        Map<Integer, Integer> idxMap = new HashMap<>();
+        for (int i = 0; i < flips.length; i++) {
+            idxMap.put(flips[i], i);
+        }
+        int r = 0, count = 0;
+        for (int i = 0; i < flips.length ; i++) {
+            visited[idxMap.get(i+1)] = true;
+            while (r <= i) {
+                if (!visited[r]) {
+                    break;
+                }
+                if (r == i) {
+                    count++;
+                    break;
+                }
+                r++;
+            }
+        }
+        return count;
+    }
+}
